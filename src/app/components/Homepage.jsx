883537f6919de70514ccc905fa4858d5fe00be75
Ashutosh lakshakar <lakshakarashutosh@gmail.com>
2024-04-04 15:19:39 +0530

    feat:fix bugs

--- src/app/components/Homepage.jsx
+++ src/app/components/Homepage.jsx
@@ -15,7 +15,8 @@ export const Homepage = () => {
     <>
       <div className="w-[100%]   flex justify-start  border-black   items-center h-screen">
         <button
-          className="bg-blue-500 absolute left-10 top-20 text-white px-4 py-2 rounded"
+          className="bg-gray-400 text-white z-10 border-2 border-white-400 absolute
+           left-10 top-20  px-8 py-3 rounded"
           onClick={togglePopup}
         >
           Save segment
